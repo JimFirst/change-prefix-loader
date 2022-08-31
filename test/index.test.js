57ad3a064c5e0819c65caa3650731891f43bfa3f
董铭杰 <dongmj@getui.com>
2022-08-31 17:18:06 +0800

    [update]: 单元测试优化

--- test/index.test.js
+++ test/index.test.js
@@ -4,19 +4,19 @@ const prefix = 'gp-'
 const reg = getRegExp('el-', prefix)
 
 describe('正则替换', () => {
-  it('替换el-menu', () => {
-    assert.equal(replace('el-menu', reg), 'gp-menu')
-  })
-  it('替换.el-menu', () => {
-    assert.equal(replace('.el-menu', reg), '.gp-menu')
-  })
-  it('替换carousel-arrow-left', () => {
-    assert.equal(replace('carousel-arrow-left', reg), 'carousel-arrow-left')
-  })
-  it('替换el-menu el-menu-item', () => {
-    assert.equal(replace('el-menu el-menu-item', reg), 'el-menu gp-menu-item')
-  })
+  generateIt('el-menu', 'gp-menu')
+  generateIt('.el-menu', '.gp-menu')
+  generateIt('babel-helper-vue-jsx-merge-props', 'babel-helper-vue-jsx-merge-props')
+  generateIt('carousel-arrow-left', 'carousel-arrow-left')
+  generateIt('el-menu el-menu-item', 'gp-menu gp-menu-item')
+
+  function generateIt(str, expect) {
+    it(`替换${str}`, () => {
+      assert.equal(replace(str, reg), expect)
+    })
+  }
 })
+
 
 function replace(str, reg) {
   return str.replace(reg, `$1${prefix}`)
